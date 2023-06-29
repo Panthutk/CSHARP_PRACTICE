7d2bec96478b1dff4e8c95725ddc48db41d90feb
Panthutk <panthat.k@gmail.com>
2023-06-29 13:30:20 +0700

    Lesson 1

--- Program.cs
+++ Program.cs
@@ -6,13 +6,20 @@ namespace FutureSkill
     {
         static void Main(string[] args)
         {
-            YourFunction();
+            //lesson1
+            int year = 1982;
+            string name = "Aqwq";
+            float salary = 1603.5f;
+            Console.WriteLine("his name is ;{0}", name);
+            Console.WriteLine("Born in ;{0}", year);
+            Console.WriteLine("his Salary ;{0}", salary);
+
+            Console.WriteLine("Name {0},Year {1},Salary {2}", name, year, salary);
+
+
+
         }
 
-        static void YourFunction()
-        {
-            Console.WriteLine("Hello world");
-            Console.ReadKey();
-        }
+
     }
 }
